722b6fec931035df75d110edd054359aed5566ad
윤서 <Yunse070708@gmail.com>
2024-12-22 07:19:14 +0900

    feat : 퍼즐

--- src/pages/invite.jsx
+++ src/pages/invite.jsx
@@ -5,6 +5,19 @@ import { useParams } from "react-router-dom";
 
 const Invite = () => {
   const param = useParams();
+  const link = `http://localhost:5173/puzzles/${param.id}`;
+
+  const handleCopy = () => {
+    navigator.clipboard
+      .writeText(link)
+      .then(() => {
+        alert("링크가 복사되었습니다!");
+      })
+      .catch((error) => {
+        console.error("복사 실패:", error);
+        alert("링크 복사에 실패했습니다.");
+      });
+  };
 
   return (
     <>
@@ -12,8 +25,8 @@ const Invite = () => {
       <div className="Layout_invite">
         <span className="send_invite">함께할 친구들을 초대해보세요!</span>
         <span className="share">
-          <p className="textBox">http://localhost:5173/puzzles/{param.id}</p>
-          <img src={copy} />
+          <p className="textBox">{link}</p>
+          <img src={copy} alt="Copy" onClick={handleCopy} />
         </span>
       </div>
     </>
